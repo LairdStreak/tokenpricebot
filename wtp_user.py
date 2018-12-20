0204dbaf3f965232ee8eedf225bc77e7ed622bc1
Laird Streak <laird.streak@seequent.com>
2018-12-20 14:38:17 +1300

    Dont call all methods.

--- wtp_user.py
+++ wtp_user.py
@@ -8,8 +8,8 @@ def main():
     client_ID = os.getenv('BNCLIENT')
     client_Secret = os.getenv('BNSECRET')
     adapter = wow_api_adapter(client_ID, client_Secret)
-    response = adapter.get_recent_auctions()
-    auc_uri = response['files'][0]['url']
+    # response = adapter.get_recent_auctions()
+    # auc_uri = response['files'][0]['url']
     guildresponse = adapter.get_guild_data()
     print(guildresponse)
 
