0e029aba43f2ebeca9bf9d5582c62ee7a06080a6
Laird Streak <laird.streak@seequent.com>
2018-12-27 10:30:00 +1300

    Convert from dict to pd Dataframe

--- wow_api_adapter.py
+++ wow_api_adapter.py
@@ -1,4 +1,5 @@
 from wowapi import WowApi
+import pandas as pd
 
 class wow_api_adapter:
 
@@ -7,8 +8,19 @@ class wow_api_adapter:
         self.CLIENT_SECRET = client_Secret
         self.DATAAPI = WowApi(self.CLIENT_ID, self.CLIENT_SECRET)
 
+    def get_character_classes(self):
+        class_dict = self.DATAAPI.get_character_classes(region='us')
+        df = pd.DataFrame.from_dict(class_dict['classes'])
+        # df[df['id'] == 2].name
+        return df
+
+    def get_gender_description(self, gender):
+        return 'Male' if gender == 0 else 'Female'
+
     def get_recent_auctions(self):
         return self.DATAAPI.get_auctions('us', 'khazgoroth', locale='enUS')
 
     def get_guild_data(self):
-        return self.DATAAPI.get_guild_profile('us', 'dathremar', guild_name='whitehand', fields='members') # /dathremar/WhiteHand/
+        guild_profile = self.DATAAPI.get_guild_profile('us', 'dathremar', guild_name='whitehand', fields='members') # /dathremar/WhiteHand/
+        dfmembers = pd.DataFrame.from_dict(guild_profile['members'])
+        return dfmembers
--- wtp_user.py
+++ wtp_user.py
@@ -2,15 +2,19 @@ import os
 from dotenv import load_dotenv
 from wow_token_price_fetcher import get_token_price
 from wow_api_adapter import wow_api_adapter
+import pprint
 
 def main():
     load_dotenv(verbose=True)
     client_ID = os.getenv('BNCLIENT')
     client_Secret = os.getenv('BNSECRET')
     adapter = wow_api_adapter(client_ID, client_Secret)
+    #classList = adapter.get_character_classes()
+    #pprint.pprint(classList)
     # response = adapter.get_recent_auctions()
     # auc_uri = response['files'][0]['url']
     guildresponse = adapter.get_guild_data()
+    pprint.pprint(guildresponse)
     print(guildresponse)
 
 if __name__ == '__main__':
